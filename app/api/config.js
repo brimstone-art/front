468dd9d15c918971ac9e3ac57d58b8efb3ba0a56
brimstone-art <149525088+brimstone-art@users.noreply.github.com>
2024-05-24 17:20:20 +0300

    перепопытка

--- app/api/config.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = 'https://br-domain.nomoredomainswork.ru/';
-
-export const endpoints = {
-  games: `${BASE_URL}/games`,
-  auth: `${BASE_URL}/auth/local`,
-  me: `${BASE_URL}/users/me`
-};
